f1e6b5e653c427c0813b44c21c3fdeb95e56e9a7
chad <chadshankweiler@gmail.com>
2024-02-26 21:02:29 -1000

    Create file.ino

---
+++ file.ino
@@ -0,0 +1,117 @@
+void setup() {
+  
+pinMode(2, OUTPUT);
+pinMode(3, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(5, OUTPUT);
+pinMode(6, OUTPUT);
+pinMode(7, OUTPUT);
+pinMode(8, OUTPUT);
+pinMode(9, OUTPUT);
+pinMode(10, OUTPUT);
+pinMode(11, OUTPUT);
+pinMode(12, OUTPUT);
+pinMode(13, OUTPUT);
+pinMode(A2, OUTPUT);
+pinMode(A3, OUTPUT);
+pinMode(A4, OUTPUT);
+pinMode(A5, OUTPUT);
+}
+
+void loop() {
+  
+delay(20);
+digitalWrite(12, LOW);
+digitalWrite(A2, LOW);
+digitalWrite(A3, HIGH);
+digitalWrite(A4, HIGH);
+digitalWrite(A5, HIGH);
+digitalWrite(13, LOW);
+digitalWrite(11, LOW);
+digitalWrite(10, LOW);
+digitalWrite(9, HIGH);
+digitalWrite(8, HIGH);
+digitalWrite(7, LOW);
+digitalWrite(6, HIGH);
+digitalWrite(5, LOW);
+digitalWrite(4, HIGH);
+digitalWrite(3, LOW);
+digitalWrite(2, LOW);
+
+delay(55);
+digitalWrite(12, LOW);
+digitalWrite(A2, HIGH);
+digitalWrite(A3, LOW);
+digitalWrite(A4, LOW);
+digitalWrite(A5, LOW);
+digitalWrite(13, LOW);
+digitalWrite(11, HIGH);
+digitalWrite(10, HIGH);
+digitalWrite(9, HIGH);
+digitalWrite(8, HIGH);
+digitalWrite(7, LOW);
+digitalWrite(6, HIGH);
+digitalWrite(5, LOW);
+digitalWrite(4, HIGH);
+digitalWrite(3, HIGH);
+digitalWrite(2, HIGH);
+
+delay(40);
+digitalWrite(12, HIGH);
+digitalWrite(A2, LOW);
+digitalWrite(A3, HIGH);
+digitalWrite(A4, LOW);
+digitalWrite(A5, LOW);
+digitalWrite(13, LOW);
+digitalWrite(11, HIGH);
+digitalWrite(10, HIGH);
+digitalWrite(9, HIGH);
+digitalWrite(8, HIGH);
+digitalWrite(7, HIGH);
+digitalWrite(6, HIGH);
+digitalWrite(5, LOW);
+digitalWrite(4, LOW);
+digitalWrite(3, HIGH);
+digitalWrite(2, LOW);
+
+delay(20);
+digitalWrite(12, LOW);
+digitalWrite(A2, LOW);
+digitalWrite(A3, HIGH);
+digitalWrite(A4, HIGH);
+digitalWrite(A5, HIGH);
+digitalWrite(13, LOW);
+digitalWrite(11, HIGH);
+digitalWrite(10, HIGH);
+digitalWrite(9, HIGH);
+digitalWrite(8, LOW);
+digitalWrite(7, LOW);
+digitalWrite(6, HIGH);
+digitalWrite(5, LOW);
+digitalWrite(4, HIGH);
+digitalWrite(3, HIGH);
+digitalWrite(2, LOW);
+
+delay(10);
+digitalWrite(12, LOW);
+digitalWrite(A2, LOW);
+digitalWrite(A3, HIGH);
+digitalWrite(A4, LOW);
+digitalWrite(A5, LOW);
+digitalWrite(13, HIGH);
+digitalWrite(11, HIGH);
+digitalWrite(10, HIGH);
+digitalWrite(9, HIGH);
+digitalWrite(8, LOW);
+digitalWrite(7, LOW);
+digitalWrite(6, HIGH);
+digitalWrite(5, HIGH);
+digitalWrite(4, HIGH);
+digitalWrite(3, HIGH);
+digitalWrite(2, LOW);
+
+
+
+
+
+}
